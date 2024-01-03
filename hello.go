9c800923079168617b5edd7de9fc7dec9f936169
wave-rider <wave-rider@users.noreply.github.com>
2024-01-04 11:26:56 +1300

    Added a conditional switch-case example

--- hello.go
+++ hello.go
@@ -2,6 +2,22 @@ package main
 import "fmt"
 import "rsc.io/quote"
 
+func frequencies() {
+	var f int
+	fmt.Scanln(&f)
+	//your code goes here
+	switch {
+	  case f >0 && f<20:
+		fmt.Println("Infrasound")
+	  case f>=20 && f<=20000:
+		fmt.Println("Audible")
+	  case f>20000:
+		fmt.Println("Ultrasound")
+	  default:
+		fmt.Println("Wrong Input")
+	}
+  }
+
 func main() {
 	fmt.Println("Hello, World!")
 	x := 42
@@ -11,5 +27,6 @@ func main() {
 	}
 	// From https://go.dev/doc/tutorial/getting-started#install
 	fmt.Println(quote.Go())
+	frequencies()
 
 }
